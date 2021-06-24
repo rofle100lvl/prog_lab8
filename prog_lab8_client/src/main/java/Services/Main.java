9ea74516c6a072d23026a20984c48da9533d1785
rofle100lvl <45801227+rofle100lvl@users.noreply.github.com>
2021-06-24 19:01:30 +0300

    task2 added network service

---
+++ prog_lab8_client/src/main/java/Services/Main.java
@@ -0,0 +1,9 @@
+package Services;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Service service = new Service();
+        service.login("login","pass");
+    }
+
+}
